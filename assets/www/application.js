621abd323afc3bbe25158512f6af17ae5546a5f1
V Sreekanth <sreeix@gmail.com>
2011-02-18 02:35:48 +0530

    Post instead of put to avoid proxy issues.

--- assets/www/application.js
+++ assets/www/application.js
@@ -1,16 +1,29 @@
 var startRider = function(){
  	navigator.network.isReachable("racetimer.heroku.com", function(reachability) {
-    // navigator.notification.alert("test");
-
     var riderNumber = document.getElementById("rider_number").value;
     if(reachability == NetworkStatus.NOT_REACHABLE){
       navigator.notification.alert("Network is not up");
     }else {
-      $.post("http://racetimer.heroku.com/rider.json", {number: riderNumber, start_time: new Date()}, function(data) {
-       navigator.notification.alert("Posted...");
+      $.post("http://racetimer.heroku.com/rider.json", {number: riderNumber, start_time: (new Date().toString())}, function(data) {
+       navigator.notification.alert("Posted... "+ new Date().toString());
       });
     }
-    
+ 	});
+ 	
+};
+
+var finishRider = function(){
+  navigator.network.isReachable("racetimer.heroku.com", function(reachability) {
+    var riderNumber = document.getElementById("rider_number").value;
+    if(reachability == NetworkStatus.NOT_REACHABLE){
+      navigator.notification.alert("Network is not up");
+    }else {
+      navigator.notification.alert("PUT... "+ new Date().toString());
+      $.post("http://racetimer.heroku.com/rider/"+riderNumber+".json", {end_time: (new Date().toString())} ,function(data) {
+        
+        navigator.notification.alert("Time taken ... "+ data.time_taken + " seconds");
+      });
+    }
  	});
 };
 
--- src/com/activesphere/hello/TimerApp.java
+++ src/com/activesphere/hello/TimerApp.java
@@ -1,9 +1,8 @@
 package com.activesphere.hello;
 
-import com.phonegap.DroidGap;
-
-import android.app.Activity;
 import android.os.Bundle;
+
+import com.phonegap.DroidGap;
 
 public class TimerApp extends DroidGap {
     /** Called when the activity is first created. */
@@ -11,6 +10,5 @@ public class TimerApp extends DroidGap {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         super.loadUrl("file:///android_asset/www/index.html");
-//        setContentView(R.layout.main);
     }
 }
